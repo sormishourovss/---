5c11bea3bf993405a6e7b17d800d8ec0bb62327d
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-03-09 11:10:03 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -4,9 +4,9 @@ module.exports.config = {
     permission: 0,
     credits: "nayan",
     prefix: true,
-    description: "search results on google",
-    category: "without prefix",
-    usages: "google [text]",
+    description: "",
+    category: "prefix",
+    usages: "",
     cooldowns: 5,
     dependencies: 
 	{
@@ -28,15 +28,14 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
-𝐍𝐚𝐦𝐞       : 𝐅𝐚𝐫𝐡𝐚𝐧 𝐈𝐬𝐥𝐚𝐦
+𝐍𝐚𝐦𝐞       : RIFAT ISLAM
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
- 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐁𝐚𝐫𝐢𝐬𝐡𝐚𝐥
 𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
 𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
 𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: 𝐈𝐧𝐛𝐨𝐱
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/profile.php?id=100069926588234&mibextid=b06tZ0`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 :https://www.facebook.com/REAL.SYSTEM.ERROR.KING`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
-      return request(encodeURI(`https://graph.facebook.com/100069926588234/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
+      return request(encodeURI(`https://graph.facebook.com/743289921/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
    };
