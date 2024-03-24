cd9b66cb63c4359e02f49629126b8d734c0db230
RAKIB CHOWDHURY <152077471+SYSTEM-ERROR-KING@users.noreply.github.com>
2024-03-24 11:10:17 +0600

    Create Rakib.js

---
+++ scripts/commands/Rakib.js
@@ -0,0 +1,28 @@
+const fs = require("fs");
+module.exports.config = {
+	    name: "😋",
+    version: "1.1.0",
+    permission: 0,
+    credits: "farhan",
+    description: "noprefix",
+    prefix: true,
+    category: "commands",
+    usages: "😜",
+    cooldowns: 5,
+};
+
+module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
+	var { threadID, messageID } = event;
+	if (event.body.indexOf("রাকিব চৌধুরী")==0 || event.body.indexOf("@Rakib Chowdhury")==0 || event.body.indexOf("BOSS")==0 ||
+event.body.indexOf("boss")==0) {
+		var msg = {
+				body: "~ আমার মালিক https://www.facebook.com/SYSTEM.ERROR.KING?mibextid=ZbWKwL এখন বিজি আছে যা বলার সন্ধা ৬ টার পরে বলবেন ধন্যবাদ..!!",
+				attachment: fs.createReadStream(__dirname + `/c/kingrakibokh.mp4`)
+			}
+			api.sendMessage( msg, threadID, messageID);
+    api.setMessageReaction("😻", event.messageID, (err) => {}, true)
+		}
+	}
+	module.exports.run = function({ api, event, client, __GLOBAL }) {
+
+  }
