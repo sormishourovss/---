89fd080103b9b30e4cefe5726f04b222d6662efa
𝐊𝐈𝐍𝐆 𝐒𝐇𝐎𝐔𝐑𝐎𝐕 𝐁𝐎𝐓 <164044359+sormishourovss@users.noreply.github.com>
2024-08-24 19:58:29 +0600

    Update ig.js

--- scripts/commands/ig.js
+++ scripts/commands/ig.js
@@ -59,6 +59,6 @@ const fs = global.nodemodule["fs-extra"];
 
   
 ];
-	 var callback = () => api.sendMessage({body:`「 ${know} 」`,attachment: fs.createReadStream(__dirname + "/cache/5.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.jpg"));	
+	s var callback = () => api.sendMessage({body:`「 ${know} 」`,attachment: fs.createReadStream(__dirname + "/cache/5.jpg")}, event.threadID, () => fs.unlinkSync(__dirname + "/cache/5.jpg"));	
       return request(encodeURI(link[Math.floor(Math.random() * link.length)])).pipe(fs.createWriteStream(__dirname+"/cache/5.jpg")).on("close",() => callback());
    };
